f61d7ee410fe42a9247ca5ce2a936f8f836388d9
Ken Ruf <ruf.kenichi@gmail.com>
2020-03-22 21:07:55 -0500

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,14 +10,26 @@ class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
+    def __str__(self):
+        return f'({self.source}, {self.destination})'
 
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = [None] * (length - 1)
 
-    """
-    YOUR CODE HERE
-    """
+    current_ticket = None
+    for ticket in tickets:
+        if ticket.source == 'NONE':
+            current_ticket = ticket
+        hash_table_insert(hashtable, ticket.source, ticket)
 
-    pass
+    index = 0
+    while current_ticket.destination != "NONE":
+        print(current_ticket)
+        route[index] = current_ticket.destination
+        next_ticket = hash_table_retrieve(hashtable, current_ticket.destination)
+        index += 1
+        current_ticket = next_ticket
+
+    return route
